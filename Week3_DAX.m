57aff9437f79061634e7df6e16d0a4ad861cbe46
Sheikh Najam <64066796+nds-najam@users.noreply.github.com>
2024-08-02 06:47:54 +0530

    Update Week3_DAX.m

--- Week3_DAX.m
+++ Week3_DAX.m
@@ -1,1 +1,17 @@
 numOrders = COUNTROWS('Sales Orders Sheet')
+Total Cost = 'Sales Orders Sheet'[Unit Cost] * 'Sales Orders Sheet'[Order Quantity]
+
+#
+# Calculate the Net Profit Margin = (Total Revenue - Total Cost)*100/(Total Revenue)
+#
+Net Profit Margin = 
+VAR _Revenue = SUM('Sales Orders Sheet'[Sales Amount])
+VAR _Cost = SUM('Sales Orders Sheet'[Total Cost])
+VAR _NetProfit = 
+DIVIDE(
+    _Revenue - _Cost,
+    _Revenue,
+    0
+)
+RETURN _NetProfit
+
